a455d172235b36a748445861cd6251e8edd04319
Jayson Ragasa <jayson.d.ragasa@live.com>
2022-11-12 20:35:12 +0800

    Added MAUI but there's an error in AndroidManifest file

---
+++ GoogleDriveIIntegration/MauiGoogleDriveIntegration/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MauiGoogleDriveIntegration
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ GoogleDriveIIntegration/MauiGoogleDriveIntegration/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MauiGoogleDriveIntegration
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
